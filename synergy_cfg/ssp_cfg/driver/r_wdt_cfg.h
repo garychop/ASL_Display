59395e30bff8b8d4d8214d49b57e77dbf064cb13
GaryChop <gchop@hotmail.com>
2019-10-27 17:23:38 -0400

    Initial Commit

---
+++ synergy_cfg/ssp_cfg/driver/r_wdt_cfg.h
@@ -0,0 +1,5 @@
+/* generated configuration header file - do not edit */
+#ifndef R_WDT_CFG_H_
+#define R_WDT_CFG_H_
+#define WDT_CFG_PARAM_CHECKING_ENABLE ((1))
+#endif /* R_WDT_CFG_H_ */
---
+++ synergy_cfg/ssp_cfg/framework/sf_message_cfg.h
@@ -0,0 +1,6 @@
+/* generated configuration header file - do not edit */
+#ifndef SF_MESSAGE_CFG_H_
+#define SF_MESSAGE_CFG_H_
+#define SF_MESSAGE_CFG_PARAM_CHECKING_ENABLE (1)
+#define SF_MESSAGE_CFG_QUEUE_SIZE (16 * 4)
+#endif /* SF_MESSAGE_CFG_H_ */
